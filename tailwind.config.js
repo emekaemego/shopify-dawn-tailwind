7edf43b479f51b844435cc80133dda10e0903642
Emeka Emego <emego.emeka@gmail.com>
2022-08-05 13:38:57 +0100

    add more home sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,19 @@ module.exports = {
       x2lg: '1920px',
       pageMaxWidth: '1440px',
     },
+    /* fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1.5rem',
+      'lg': '2.125rem',
+      'xl': '2.152rem',
+      '2xl': '2.5rem',
+      '3xl': '3.12rem',
+      '4xl': '3.25rem',
+      '5xl': '3.8rem',
+      '6xl': '4.7rem',
+      '7xl': '5.5rem',
+    }, */
     extend: {
       fontFamily: {
         heading: 'var(--font-heading-family)',
